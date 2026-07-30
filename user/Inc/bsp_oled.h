4da2cf932ef4a5a3fb162d4d5a2ae860c12136b6
Dusty <2908251885@qq.com>
2026-07-31 00:39:31 +0800

    bsp_oled

---
+++ user/Inc/bsp_oled.h
@@ -0,0 +1,30 @@
+//
+// Created by Dolores on 2026/7/31.
+//
+
+#ifndef CHASSIS_BSP_OLED_H
+#define CHASSIS_BSP_OLED_H
+
+#include "main.h"
+#include "i2c.h"
+#include <stdint.h>
+#include <string.h>
+
+#define OLED_I2C_HANDLE hi2c1
+#define OLED_I2C_ADDR 0x78U
+#define OLED_WIDTH 128U
+#define OLED_HEIGHT 64U
+#define OLED_PAGES (OLED_HEIGHT / 8U)
+#define OLED_REFRESH_PERIOD_MS 200U
+
+void BSP_OLED_Init(void);
+void BSP_OLED_Clear(void);
+void BSP_OLED_Fill(void);
+void BSP_OLED_ShowChar(uint8_t x, uint8_t y, char ch);
+void BSP_OLED_ShowString(uint8_t x, uint8_t y, const char *str);
+void BSP_OLED_ShowNum(uint8_t x, uint8_t y, int32_t num, uint8_t width);
+void BSP_OLED_ShowFloat(uint8_t x, uint8_t y, float num, uint8_t width, uint8_t prec);
+void BSP_OLED_DrawPoint(uint8_t x, uint8_t y, uint8_t color);
+void BSP_OLED_Refresh(void);
+
+#endif //CHASSIS_BSP_OLED_H
